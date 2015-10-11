78af2a0beb50250b38f1d0fcd18f1ebe5378abe7
Nir <forums2@walla.com>
2015-10-11 17:38:08 +0300

    ..

--- app/src/main/java/share/fair/fairshare/MainActivity.java
+++ app/src/main/java/share/fair/fairshare/MainActivity.java
@@ -1,9 +1,6 @@
 package share.fair.fairshare;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.Context;
+
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
@@ -11,18 +8,15 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.Map;
+
 
 
 public class MainActivity extends FragmentActivity  {
@@ -46,7 +40,6 @@ public class MainActivity extends FragmentActivity  {
             }
         });
         groupNames = Group.getSavedGroupNames(getApplicationContext());
-        groupNames.add(new NameAndKey("GROUPYAIR","123"));
         groupList = (ListView) findViewById(R.id.groups_list);
         groupAdapter = new GroupsAdapter(this,R.id.info, groupNames);
         groupList.setAdapter(groupAdapter);
@@ -60,7 +53,7 @@ public class MainActivity extends FragmentActivity  {
 
     }
 
-    
+
 
     public static class AddNewGroupDialog extends DialogFragment {
         public AddNewGroupDialog() {
@@ -121,5 +114,4 @@ public class MainActivity extends FragmentActivity  {
 
         }
     }
-
 }
