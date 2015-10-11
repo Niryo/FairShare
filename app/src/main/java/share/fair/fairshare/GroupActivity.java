f2b636a357d4f28a2e29fdd4e5ecbf2d2b0a7668
orish88 <orish88@gmail.com>
2015-10-11 17:30:22 +0300

    groupActivity1

---
+++ app/src/main/java/share/fair/fairshare/GroupActivity.java
@@ -0,0 +1,59 @@
+package share.fair.fairshare;
+
+import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class GroupActivity extends FragmentActivity {
+
+    TextView groupNameTextView;
+    Button addUserButton;
+    ArrayList<User> users;
+    ListView userList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group);
+        addUserButton = (Button)findViewById(R.id.bt_add_user);
+        addUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EnterNameDialog dialog = new EnterNameDialog();
+                dialog.setTitle("Choose user name:");
+                dialog.setHint("User's name");
+                dialog.show(getSupportFragmentManager(), "add_new_user");
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_group, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/share/fair/fairshare/MainActivity.java
+++ app/src/main/java/share/fair/fairshare/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
@@ -43,7 +44,10 @@ public class MainActivity extends FragmentActivity implements AdapterView.OnItem
         createNewGroupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new AddNewGroupDialog().show(getSupportFragmentManager(), "add_new_group");
+                EnterNameDialog dialog = new EnterNameDialog();
+                dialog.setTitle("Choose group name:");
+                dialog.setHint("Group's name");
+                dialog.show(getSupportFragmentManager(), "add_new_group");
 
             }
         });
@@ -58,68 +62,11 @@ public class MainActivity extends FragmentActivity implements AdapterView.OnItem
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+        Intent openGroup = new Intent(this, GroupActivity.class);
+        startActivity(openGroup);
+        finish();
     }
 
 
-    public static class AddNewGroupDialog extends DialogFragment {
-        public AddNewGroupDialog() {
-            // Empty constructor required for DialogFragment
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View dialogLayout = inflater.inflate(R.layout.new_group_dialog_layout, container);
-            setCancelable(false);
-            getDialog().setContentView(R.layout.new_group_dialog_layout);
-            getDialog().setTitle("Choose group name:");
-
-            final EditText groupNameEditText = (EditText) dialogLayout.findViewById(R.id.group_name_edit_text);
-            final Button createButton = (Button) dialogLayout.findViewById(R.id.create_button);
-            final Button cancelButton = (Button) dialogLayout.findViewById(R.id.cancel_button);
-            createButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String name = groupNameEditText.getText().toString();
-                    Group newGroup = new Group(name);
-                    newGroup.saveGroupToStorage(getContext());
-                    Log.w("custom", name + " as been created");
-                    getDialog().dismiss();
-
-                }
-            });
-            cancelButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    getDialog().dismiss();
-
-                }
-            });
-            createButton.setEnabled(false);
-            groupNameEditText.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                }
-
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-                }
-
-                @Override
-                public void afterTextChanged(Editable s) {
-                    if (groupNameEditText.getText().toString().length() > 0) {
-                        createButton.setEnabled(true);
-                    } else {
-                        createButton.setEnabled(false);
-                    }
-                }
-
-            });
-            getDialog().show();
-            return dialogLayout;
-
-        }
-    }
 
 }
