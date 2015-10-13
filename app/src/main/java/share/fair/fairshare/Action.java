c11fd5979e1f7e45f3b791d0d8be7ffb66c7e672
Nir <forums2@walla.com>
2015-10-13 21:16:48 +0300

    log added

---
+++ app/src/main/java/share/fair/fairshare/Action.java
@@ -0,0 +1,7 @@
+package share.fair.fairshare;
+
+/**
+ * Created by Nir on 13/10/2015.
+ */
+public class Action {
+}
---
+++ app/src/main/java/share/fair/fairshare/GroupLog.java
@@ -0,0 +1,11 @@
+package share.fair.fairshare;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Nir on 13/10/2015.
+ */
+public class GroupLog {
+    ArrayList<Action> actions;
+
+}
