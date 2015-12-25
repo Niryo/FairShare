61716ef9e95b5e2eb8afb941c2d6113ed95df07f
Nir <forums2@walla.com>
2015-12-25 17:09:02 +0200

    clean App activity

--- app/src/main/java/share/fair/fairshare/activities/App.java
+++ app/src/main/java/share/fair/fairshare/activities/App.java
@@ -1,12 +1,6 @@
 package share.fair.fairshare.activities;
 
-import org.acra.*;
-import org.acra.annotation.*;
-
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.SharedPreferences;
-import android.util.Log;
 
 import com.orm.SugarApp;
 import com.parse.FindCallback;
@@ -15,60 +9,53 @@ import com.parse.ParseException;
 import com.parse.ParseInstallation;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
-import com.parse.SaveCallback;
 
-import java.math.BigInteger;
-import java.security.SecureRandom;
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
 import java.util.List;
 
 import share.fair.fairshare.R;
-import share.fair.fairshare.activities.GroupActivity;
 
 
-/**
- * Created by Nir on 09/10/2015.
- */
-//@ReportsCrashes(mailTo = "niryosef89@gmail.com",
-//        mode = ReportingInteractionMode.DIALOG,
-//        resDialogText = R.string.crash_dialog_text,
-//        resDialogIcon = android.R.drawable.ic_dialog_info, //optional. default is a warning sign
-//        resDialogTitle = R.string.crash_dialog_title // optional. default is your application name
-//        )
+@ReportsCrashes(mailTo = "niryosef89@gmail.com",
+        mode = ReportingInteractionMode.DIALOG,
+        resDialogText = R.string.crash_dialog_text,
+        resDialogIcon = android.R.drawable.ic_dialog_info, //optional. default is a warning sign
+        resDialogTitle = R.string.crash_dialog_title // optional. default is your application name
+)
 public class App extends SugarApp {
-    private String currentVersion="betaV1.3";
-   public GroupActivity activity=null;
+    public GroupActivity activity = null; //holds the current groupActivity. It will indicate if the groupActivity is running or not.
+    private String currentVersion = "betaV1.3"; //the current version
 
     @Override
     public void onCreate() {
         super.onCreate();
-        ACRA.init(this);
+        ACRA.init(this); //init the crash report library
         Parse.enableLocalDatastore(getApplicationContext());
         Parse.initialize(this, "nLLyqbfak5UsJbwJ086zWMCr5Ux6RvzXOM1kBpX3", "sauupds6DzHf2EroSxBjbnORMgMLbY87UKbFW0u9");
-        Parse.setLogLevel(Parse.LOG_LEVEL_VERBOSE);
-        ParseInstallation.getCurrentInstallation().saveInBackground(new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if (e == null) {
-                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
-                } else {
-                    Log.e("com.parse.push", "failed to subscribe for push", e);
-                }
-            }
-        });
+        ParseInstallation.getCurrentInstallation().saveInBackground();
         verifyVersion();
-
-
-
-
     }
 
-    public void registerGroupActivity(GroupActivity activity){
-        this.activity=activity;
+    /**
+     * Register the current instance of groupActivity
+     * @param activity Group activity
+     */
+    public void registerGroupActivity(GroupActivity activity) {
+        this.activity = activity;
     }
 
-    private void verifyVersion (){
+    /**
+     * Verifies that we are using the latest compativle version of fairShare
+     */
+    private void verifyVersion() {
+        //we will check in parse The VERSION table to verify that we have the latest compatible
+        //version and if not, we will set in the shared preferences a value that indicates
+        // illegal version:
+
         final SharedPreferences settings = getSharedPreferences("MAIN_PREFERENCES", 0);
-
 
         ParseQuery<ParseObject> query = ParseQuery.getQuery("VERSION");
         query.findInBackground(new FindCallback<ParseObject>() {
@@ -77,7 +64,7 @@ public class App extends SugarApp {
                 if (e == null && objects != null) {
                     ParseObject parseObject = objects.get(0);
                     String lastVersion = parseObject.getString("version");
-                        SharedPreferences.Editor editor = settings.edit();
+                    SharedPreferences.Editor editor = settings.edit();
                     if (!currentVersion.equals(lastVersion)) {
                         editor.putBoolean("isLegalVersion", false);
                         editor.commit();
@@ -85,9 +72,9 @@ public class App extends SugarApp {
                         editor.putBoolean("isLegalVersion", true);
                         editor.commit();
 
+                    }
                 }
-            } }
-
+            }
 
         });
 
